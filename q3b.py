80ed1e6eb7e9d439cb402f6d1d937d826d747f37
mkursunoglu24 <mkursunoglu24@ku.edu.tr>
2026-04-02 23:18:45 +0300

    q3b.py

--- q3b.py
+++ q3b.py
@@ -2,4 +2,46 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    a1 = a[2::]
+    b1 = b[2::]
+    
+    
+    def binsum(string):
+        suma = 0
+        for i in range(len(string)):
+           if string[i]==1:
+               suma += 2**(len(string)-1-i)
+        return suma
+    
+    c = binsum(a1) + binsum(b1)
+    return c
+    
+    def maxg(gle):
+        a = 0
+        while gle>=2**(a):
+            a+=1
+        return a-1
+    
+    
+    res = ''
+    for i in range(maxg(c),0,-1):
+        if c>= 2**(maxg(c)-i):
+            res+= '1'
+            c-=2**(maxg(c)-i)
+            
+        else:
+            res +='0'
+            
+            
+    return '0b' +res
+        
+        
+            
+    
+            
+            
+
+            
+        
+            
+        
